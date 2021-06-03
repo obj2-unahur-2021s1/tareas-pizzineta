66b230103787a94611bfb546c972ed8a2d3165b4
Joel <joelyturrieta@gmail.com>
2021-06-03 00:22:00 -0300

    Cuanto tarda en finalizar una tarea

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,15 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea(val horasEstimadas: Int, val responsable: Empleado){
+    val empleadosAsignados = mutableListOf<Empleado>()
+
+    fun asignarEmpleado(unEmpleado: Empleado) =
+        empleadosAsignados.add(unEmpleado)
+
+    fun cantidadDeAsignados() =
+        empleadosAsignados.size
+
+    fun horasNecesariasParaFinalizar() =
+        (this.horasEstimadas / this.cantidadDeAsignados())
+
+}
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,25 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
+    val empleado1 = Empleado(66)
+    val empleado2 = Empleado(45)
+    val empleado3 = Empleado(62)
+    val empleado4 = Empleado(79)
+    val empleado5 = Empleado(55)
+    val tarea1 = Tarea(7, empleado1)
+    tarea1.asignarEmpleado(empleado2)
+    tarea1.asignarEmpleado(empleado3)
+    tarea1.asignarEmpleado(empleado4)
+    tarea1.asignarEmpleado(empleado5)
+
+    describe("saber cuanto tarda en finalizar una tarea"){
+      it("cantidad de horas estimadas / cantidad de empleados asignados"){
+        tarea1.horasNecesariasParaFinalizar().shouldBe(1)
+      }
+    }
   }
 })
